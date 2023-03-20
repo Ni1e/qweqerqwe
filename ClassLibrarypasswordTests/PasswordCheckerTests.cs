89a267af90e408ef691a1cc674efee9d5b0f6a91
Ni1e <118896347+Ni1e@users.noreply.github.com>
2023-03-20 14:35:29 +0300

    Add files via upload

---
+++ ClassLibrarypasswordTests/PasswordCheckerTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrarypassword;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrarypassword.Tests
+{
+    [TestClass()]
+    public class PasswordCheckerTests
+    {
+        [TestMethod()]
+        public void Check_85ymbols_ReturnsTrue()
+        {
+            // Arrange.
+            string password = "ASqw12$$";
+            bool expected = true;
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void Check_45ymbols_ReturnsFalse()
+        {
+            // Arrange.
+            string password = "ASDQWE123$";
+            bool expected = false;
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+    
