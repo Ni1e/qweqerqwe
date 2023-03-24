59155131f0458e3bfc71af3ff4120bd4f7a5ef9e
Ni1e <118896347+Ni1e@users.noreply.github.com>
2023-03-24 09:50:42 +0300

    Add files via upload

---
+++ начало.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+//#include <stdafx.h>
+//#include <windows.h>
+using namespace std;
+int main()
+{
+     setlocale(LC_ALL, "Russian");
+    int a, b, z, Alekhin, Streltsov, Tomin;
+    cout<<"Введите стартовый номер" << " ";
+    cin >> a;
+    cout <<"Введите время старта" << " ";
+    cin >> b;
+    cout <<"Введите время финиша" << " ";
+    cin >> z;
+    return 0;
+}
